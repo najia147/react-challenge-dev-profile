f10b05caf6eb4929a6b479bf8b96d331992d61ad
najia <anajia231@gmail.com>
2024-10-13 22:52:50 +0430

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+function App() {
+  return (
+    <div>
+      hello reactjs
+    </div>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
