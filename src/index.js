44de00e355e5cdb7a8e6f949a5d52695cb84f681
najia <anajia231@gmail.com>
2024-10-14 18:55:42 +0430

    created components

--- src/index.js
+++ src/index.js
@@ -5,10 +5,37 @@ import './index.css';
 
 function App() {
   return (
-    <div>
-      hello reactjs
-    </div>
+    <main>
+      <Avatar />
+      <Intro />
+      <SkillList />
+    </main>
   );
+}
+
+function Avatar() {
+  return <img src='images/68281489.jpeg'/>
+}
+
+  function Intro() {
+  return (
+   <div>
+   <h1>Najia Ahmadi</h1>
+   <p>I am a web developer and I work hard to acheive my goals. That's not easy but I try a lot to reach my goals. I am interested in studying and cooking in freetimes. </p>
+   </div>
+  )
+}
+
+function SkillList() {
+ return (
+  <div>
+    <Skill />
+  </div>
+ )
+}
+
+function Skill() {
+  return <div>React 👌</div>
 }
 
 
